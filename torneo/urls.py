2be4674eddbd38ec820094b93fa1e18e92bbfe8e
coni258 <constanzacardenas.b@gmail.com>
2022-11-23 12:45:38 -0300

    listar equipos

--- torneo/urls.py
+++ torneo/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
   path('', views.showteams),
-  path('create', views.createteam, name='create_team')
+  path('form/team/done', views.createteam, name='create_team'),
+  path('form/team', views.teamform, name='form_team'),
+  path('delete <int:team_id>',views.deleteteam, name='delete_team'),
+  path('list/teams', views.listarteams, name='list_team')
 ]
--- torneo/views.py
+++ torneo/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Equipos
+from .models import Jugadores
+from .models import Staff
 
 # Create your views here.
 def showteams(request):
@@ -8,9 +10,27 @@ def showteams(request):
 def teamform(request):
   return render(request, 'team_form.html')
 
+def listarteams(request):
+  equipos = Equipos.objects.all()
+  return render(request, 'list_teams.html', {'Equipos': equipos})
+
 def createteam(request):
-  equipo = Equipos(nombre=request.POST['nombre'], abreviatura=request.POST["abreviatura"], aniofundacion=request.POST["aniofundacion"])
+  print("hola")
+  equipo = Equipos(nombre=request.POST['nombre'], abreviatura=request.POST['abreviatura'], aniofundacion=request.POST['aniofundacion'])
   equipo.save()
   return redirect("/")
 
+def deleteteam(request,team_id):
+  print(team_id)
+  return redirect("/")
 
+def createjugadores(request):
+  jugador =Jugadores(num_pasaporte=request.POST['num_pasaporte'],nombre=request.POST['nombre'],nacionalidad=request.POST['nacionalidad'],edad=request.POST['edad'],posicion=request.POST['posicion'],abr_posicion=request.POST['abr_posicion'],num_dorsal=request.POST['num_dorsal'])
+  jugador.save()
+  return redirect ("/")
+
+
+def createstaff(request):
+  staff =Staff(num_pasaporte=request.POST['num_pasaporte'],nombre=request.POST['nombre'],nacionalidad=request.POST['nacionalidad'],edad=request.POST['edad'],cargo=request.POST['cargo'],abr_cargo=request.POST['abr_cargo'])
+  staff.save()
+  return redirect ("/")
